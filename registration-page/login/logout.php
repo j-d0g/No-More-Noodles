bf052a4a8a160c93f0a8630f09a6f925ee495078
b53071 <radu.nitu@student.manchester.ac.uk>
2021-03-18 09:41:52 +0000

    logout done

--- registration-page/login/logout.php
+++ registration-page/login/logout.php
@@ -6,6 +6,6 @@ if(isset($_SESSION['user_id']))
 	unset($_SESSION['user_id']);
 }
 
-header("Location: /GitRepos/first_group_project/registration-page/registration.php");
+header("Location: /GitRepos/first_group_project/index.php");
 die;
  ?>
